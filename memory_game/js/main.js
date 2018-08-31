25a62b33db4e951c07e28d0d4b10b5a5aa1b390a
Josie Peralta <josieperalta@joezsmac.home>
2018-08-31 09:31:03 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-var cardOne = "Queen";
+/*var cardOne = "Queen";
 var cardTwo = "King";
 var cardThree = "King";
 var cardFour = "Queen";
 console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+console.log("User flipped" + cardTwo);*/
+var cards = ["queen", "queen", "king", "King"];
+cards;
+var cardsInPlay = []
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2) {
+  //Condition will be true
+} else if (cardsInPlay[0] === cardsInPlay[2]) {
+  result = alert("you found a match!");
+} else {
+  result = alert("Sorry, try again");
+}
